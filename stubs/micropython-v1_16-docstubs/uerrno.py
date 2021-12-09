6c45912e0fb1f810fa55e7d6a2bac7333bf34937
Josverl <Josverl@users.noreply.github.com>
2021-12-09 01:54:33 +0000

    add/update MicroPython v1.16 documentation stubs

---
+++ stubs/micropython-v1_16-docstubs/uerrno.py
@@ -0,0 +1,40 @@
+"""
+system error codes. See: https://docs.micropython.org/en/v1.16/library/uerrno.html
+
+|see_cpython_module| :mod:`python:errno` https://docs.python.org/3/library/errno.html .
+
+This module provides access to symbolic error codes for `OSError` exception.
+A particular inventory of codes depends on :term:`MicroPython port`.
+"""
+
+# source version: v1.16
+# origin module:: micropython/docs/library/uerrno.rst
+from typing import IO, Any, Callable, Coroutine, Dict, Generator, Iterator, List, NoReturn, Optional, Tuple, Union
+#     Error codes, based on ANSI C/POSIX standard. All error codes start with
+#     "E". As mentioned above, inventory of the codes depends on
+#     :term:`MicroPython port`. Errors are usually accessible as ``exc.errno``
+#     where ``exc`` is an instance of `OSError`. Usage example::
+# 
+#         try:
+#             uos.mkdir("my_dir")
+#         except OSError as exc:
+#             if exc.errno == uerrno.EEXIST:
+#                 print("Directory already exists")
+EEXIST : Any
+#     Error codes, based on ANSI C/POSIX standard. All error codes start with
+#     "E". As mentioned above, inventory of the codes depends on
+#     :term:`MicroPython port`. Errors are usually accessible as ``exc.errno``
+#     where ``exc`` is an instance of `OSError`. Usage example::
+# 
+#         try:
+#             uos.mkdir("my_dir")
+#         except OSError as exc:
+#             if exc.errno == uerrno.EEXIST:
+#                 print("Directory already exists")
+EAGAIN : Any
+#     Dictionary mapping numeric error codes to strings with symbolic error
+#     code (see above)::
+# 
+#         >>> print(uerrno.errorcode[uerrno.EEXIST])
+#         EEXIST
+errorcode : Dict
