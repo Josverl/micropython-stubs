d5736a3bccd23d9d71830b05a119f6638404b7f7
Josverl <Josverl@users.noreply.github.com>
2022-01-12 01:58:02 +0000

    add/update MicroPython Latest documentation stubs

--- stubs/micropython-latest-docstubs/struct.py
+++ stubs/micropython-latest-docstubs/struct.py
@@ -12,35 +12,45 @@ on the floating-point support).
 
 # source version: latest
 # origin module:: micropython/docs/library/struct.rst
-from typing import IO, Any, Callable, Coroutine, Dict, Generator, Iterator, List, NoReturn, Optional, Tuple, Union
+from typing import Any, Tuple
+
+
 def calcsize(fmt) -> int:
     """
-       Return the number of bytes needed to store the given *fmt*.
+    Return the number of bytes needed to store the given *fmt*.
     """
     ...
+
+
 def pack(fmt, v1, v2, *args) -> bytes:
     """
-       Pack the values *v1*, *v2*, ... according to the format string *fmt*.
-       The return value is a bytes object encoding the values.
+    Pack the values *v1*, *v2*, ... according to the format string *fmt*.
+    The return value is a bytes object encoding the values.
     """
     ...
+
+
 def pack_into(fmt, buffer, offset, v1, v2, *args) -> Any:
     """
-       Pack the values *v1*, *v2*, ... according to the format string *fmt*
-       into a *buffer* starting at *offset*. *offset* may be negative to count
-       from the end of *buffer*.
+    Pack the values *v1*, *v2*, ... according to the format string *fmt*
+    into a *buffer* starting at *offset*. *offset* may be negative to count
+    from the end of *buffer*.
     """
     ...
+
+
 def unpack(fmt, data) -> Tuple:
     """
-       Unpack from the *data* according to the format string *fmt*.
-       The return value is a tuple of the unpacked values.
+    Unpack from the *data* according to the format string *fmt*.
+    The return value is a tuple of the unpacked values.
     """
     ...
+
+
 def unpack_from(fmt, data, offset=0, /) -> Tuple:
     """
-       Unpack from the *data* starting at *offset* according to the format string
-       *fmt*. *offset* may be negative to count from the end of *buffer*. The return
-       value is a tuple of the unpacked values.
+    Unpack from the *data* starting at *offset* according to the format string
+    *fmt*. *offset* may be negative to count from the end of *buffer*. The return
+    value is a tuple of the unpacked values.
     """
     ...
