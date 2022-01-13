8707afeb4c57406083d1c519b1ef488085d42510
Jos Verlinde <jos.verlinde@microsoft.com>
2022-01-13 16:54:15 +0100

    add v1.17 pyboard

---
+++ stubs/micropython-v1_17-pyboard/zlib.py
@@ -0,0 +1,22 @@
+"""
+Module: 'zlib' on micropython-v1.17-pyboard
+"""
+# MCU: {'ver': 'v1.17', 'port': 'pyboard', 'arch': 'armv7emsp', 'sysname': 'pyboard', 'release': '1.17.0', 'name': 'micropython', 'mpy': 7685, 'version': '1.17.0', 'machine': 'PYBv1.1 with STM32F405RG', 'build': '', 'nodename': 'pyboard', 'platform': 'pyboard', 'family': 'micropython'}
+# Stubber: 1.5.2
+from typing import Any
+
+
+class DecompIO:
+    ''
+    def read(self, *args) -> Any:
+        ...
+
+    def readinto(self, *args) -> Any:
+        ...
+
+    def readline(self, *args) -> Any:
+        ...
+
+def decompress(*args) -> Any:
+    ...
+
