f2abf26bd1bc06811677872632097c1599272d68
Jos Verlinde <jos.verlinde@microsoft.com>
2022-01-13 16:10:42 +0100

    stubquality: add typing fallback for testing of frozen modules

---
+++ stubs/typing_fallback/_onewire.py
@@ -0,0 +1,25 @@
+"""
+Module: '_onewire' on micropython-v1.17-pyboard
+"""
+# MCU: {'ver': 'v1.17', 'port': 'pyboard', 'arch': 'armv7emsp', 'sysname': 'pyboard', 'release': '1.17.0', 'name': 'micropython', 'mpy': 7685, 'version': '1.17.0', 'machine': 'PYBv1.1 with STM32F405RG', 'build': '', 'nodename': 'pyboard', 'platform': 'pyboard', 'family': 'micropython'}
+# Stubber: 1.5.2
+from typing import Any
+
+def crc8(*args) -> Any:
+    ...
+
+def readbit(*args) -> Any:
+    ...
+
+def readbyte(*args) -> Any:
+    ...
+
+def reset(*args) -> Any:
+    ...
+
+def writebit(*args) -> Any:
+    ...
+
+def writebyte(*args) -> Any:
+    ...
+
---
+++ stubs/typing_fallback/readme.md
@@ -0,0 +1,8 @@
+# typing fallback
+
+This folder contains a set of stub files that are used to test / validate the correctness of the frozen stubs.
+
+it is built using a subset of the firmware stubs from: 
+- micropython-v1_17-esp32
+- micropython-v1_17-pybv11
+
