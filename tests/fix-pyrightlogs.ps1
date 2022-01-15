572964b29c13bd135ec07e0676f549596a67081b
Jos Verlinde <jos.verlinde@microsoft.com>
2022-01-15 23:39:08 +0100

    pyright log: update  parsing

--- tests/fix-pyrightlogs.ps1
+++ tests/fix-pyrightlogs.ps1
@@ -5,7 +5,7 @@ param (
 foreach ( $file in (Get-ChildItem $Path -Filter "pyright*.log" )) {
     Write-Host "fix : " $file.FullName
     (Get-Content $file) `
-        -replace ' - (warning|error):', ': $1:' `
+        -replace ' - (error|warning|information):', ': $1:' `
         -replace ' - (info):', ': information:' `
     | Out-File $file
 }
