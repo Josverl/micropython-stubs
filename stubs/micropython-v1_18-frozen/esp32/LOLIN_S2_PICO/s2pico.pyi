72582ad9c8d53898587ab3f6b91d6f737b1c381b
Josverl <Josverl@users.noreply.github.com>
2022-01-17 02:13:19 +0000

    add/update MicroPython v1.15 stubs

---
+++ stubs/micropython-v1_18-frozen/esp32/LOLIN_S2_PICO/s2pico.pyi
@@ -0,0 +1,16 @@
+from typing import Any
+
+SPI_MOSI: Any
+SPI_MISO: Any
+SPI_CLK: Any
+I2C_SDA: Any
+I2C_SCL: Any
+DAC1: Any
+DAC2: Any
+LED: Any
+OLED_RST: Any
+BUTTON: Any
+led: Any
+button: Any
+i2c: Any
+oled: Any
