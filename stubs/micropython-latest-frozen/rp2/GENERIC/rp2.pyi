9812b5b6335f270b9aa7aab0829f686f771ad54e
Josverl <Josverl@users.noreply.github.com>
2022-02-24 08:45:26 +0000

    add/update MicroPython Latest frozen stubs

--- stubs/micropython-latest-frozen/rp2/GENERIC/rp2.pyi
+++ stubs/micropython-latest-frozen/rp2/GENERIC/rp2.pyi
@@ -18,7 +18,20 @@ class PIOASMEmit:
     prog: Any
     wrap_used: bool
     sideset_count: int
-    def __init__(self, *, out_init: Any | None = ..., set_init: Any | None = ..., sideset_init: Any | None = ..., in_shiftdir: int = ..., out_shiftdir: int = ..., autopush: bool = ..., autopull: bool = ..., push_thresh: int = ..., pull_thresh: int = ..., fifo_join: int = ...) -> None: ...
+    def __init__(
+        self,
+        *,
+        out_init: Any | None = ...,
+        set_init: Any | None = ...,
+        sideset_init: Any | None = ...,
+        in_shiftdir: int = ...,
+        out_shiftdir: int = ...,
+        autopush: bool = ...,
+        autopull: bool = ...,
+        push_thresh: int = ...,
+        pull_thresh: int = ...,
+        fifo_join: int = ...,
+    ) -> None: ...
     delay_max: int
     sideset_opt: Any
     pass_: Any
