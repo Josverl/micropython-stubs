1bfcf82e222863adff630cc1dd53d9debc967833
Josverl <Josverl@users.noreply.github.com>
2022-02-24 16:08:29 +0000

    add/update doc-stubs

--- stubs/micropython-latest-docstubs/heapq.pyi
+++ stubs/micropython-latest-docstubs/heapq.pyi
@@ -1,5 +1,36 @@
-from typing import Any
+"""
+heap queue algorithm. See: https://docs.micropython.org/en/latest/library/heapq.html
 
-def heappush(heap, item) -> Any: ...
-def heappop(heap) -> Any: ...
-def heapify(x) -> Any: ...
+|see_cpython_module| :mod:`python:heapq` https://docs.python.org/3/library/heapq.html .
+
+This module implements the
+`min heap queue algorithm <https://en.wikipedia.org/wiki/Heap_%28data_structure%29>`_.
+
+A heap queue is essentially a list that has its elements stored in such a way
+that the first item of the list is always the smallest.
+"""
+
+# source version: latest
+# origin module:: micropython/docs/library/heapq.rst
+from typing import IO, Any, Callable, Coroutine, Dict, Generator, Iterator, List, NoReturn, Optional, Tuple, Union
+
+def heappush(heap, item) -> Any:
+    """
+    Push the ``item`` onto the ``heap``.
+    """
+    ...
+
+def heappop(heap) -> Any:
+    """
+    Pop the first item from the ``heap``, and return it.  Raise ``IndexError`` if
+    ``heap`` is empty.
+
+    The returned item will be the smallest item in the ``heap``.
+    """
+    ...
+
+def heapify(x) -> Any:
+    """
+    Convert the list ``x`` into a heap.  This is an in-place operation.
+    """
+    ...
