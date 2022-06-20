6090e7a928d4e67348f158e12609c2f167784032
Jos Verlinde <jos.verlinde@microsoft.com>
2022-06-20 12:05:58 +0200

    add Frozen Stubs for 1.19.1

---
+++ stubs/micropython-v1_19_1-frozen/esp32/LOLIN_C3_MINI/c3mini.pyi
@@ -0,0 +1,11 @@
+from typing import Any
+
+SPI_MOSI: Any
+SPI_MISO: Any
+SPI_CLK: Any
+I2C_SDA: Any
+I2C_SCL: Any
+LED: Any
+BUTTON: Any
+led: Any
+button: Any
---
+++ stubs/micropython-v1_19_1-frozen/stm32/LEGO_HUB_NO6/appupdate.pyi
@@ -0,0 +1,8 @@
+from typing import Any
+
+_SPIFLASH_UPDATE_KEY_ADDR: Any
+_SPIFLASH_UPDATE_KEY_VALUE: Any
+_FILESYSTEM_ADDR: Any
+_FILESYSTEM_LEN: Any
+
+def update_app(filename) -> None: ...
