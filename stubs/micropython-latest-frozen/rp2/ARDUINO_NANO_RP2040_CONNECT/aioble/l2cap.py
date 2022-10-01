e0dba046eca6a2b1b64c334d16fcfbad39b1ffe0
Josverl <Josverl@users.noreply.github.com>
2022-10-01 01:39:41 +0000

    add/update MicroPython latest frozen stubs

--- stubs/micropython-latest-frozen/rp2/ARDUINO_NANO_RP2040_CONNECT/aioble/l2cap.py
+++ stubs/micropython-latest-frozen/rp2/ARDUINO_NANO_RP2040_CONNECT/aioble/l2cap.py
@@ -132,10 +132,10 @@ class L2CAPChannel:
 
     # Waits until the channel is free and then sends buf.
     # If the buffer is larger than the MTU it will be sent in chunks.
-    async def send(self, buf, timeout_ms=None):
+    async def send(self, buf, timeout_ms=None, chunk_size=None):
         self._assert_connected()
         offset = 0
-        chunk_size = min(self.our_mtu * 2, self.peer_mtu)
+        chunk_size = min(self.our_mtu * 2, self.peer_mtu, chunk_size or self.peer_mtu)
         mv = memoryview(buf)
         while offset < len(buf):
             if self._stalled:
@@ -178,14 +178,14 @@ class L2CAPChannel:
 
 
 # Use connection.l2cap_accept() instead of calling this directly.
-async def accept(connection, psn, mtu, timeout_ms):
+async def accept(connection, psm, mtu, timeout_ms):
     global _listening
 
     channel = L2CAPChannel(connection)
 
     # Start the stack listening if necessary.
     if not _listening:
-        ble.l2cap_listen(psn, mtu)
+        ble.l2cap_listen(psm, mtu)
         _listening = True
 
     # Wait for the connect irq from the remote connection.
@@ -195,14 +195,14 @@ async def accept(connection, psn, mtu, timeout_ms):
 
 
 # Use connection.l2cap_connect() instead of calling this directly.
-async def connect(connection, psn, mtu, timeout_ms):
+async def connect(connection, psm, mtu, timeout_ms):
     if _listening:
         raise ValueError("Can't connect while listening")
 
     channel = L2CAPChannel(connection)
 
     with connection.timeout(timeout_ms):
-        ble.l2cap_connect(connection._conn_handle, psn, mtu)
+        ble.l2cap_connect(connection._conn_handle, psm, mtu)
 
         # Wait for the connect irq from the remote connection.
         # If the connection fails, we get a disconnect event (with status) instead.
