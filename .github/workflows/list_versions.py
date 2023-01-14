ed2cd46773b9af971a04f1ce8a2402dd396ca755
Jos Verlinde <Jos.Verlinde@Microsoft.com>
2023-01-14 20:02:43 +0100

    chore: update GHA naming

--- .github/workflows/list_versions.py
+++ .github/workflows/list_versions.py
@@ -1,4 +1,5 @@
 import json
+import os
 import sys
 from os import environ
 
@@ -6,11 +7,10 @@ from github import Github
 from packaging.version import parse
 
 
-def micropython_versions(start="v1.9.2"):
+def micropython_versions(start="v1.10"):
     g = Github()
     repo = g.get_repo("micropython/micropython")
-    return [tag.name for tag in repo.get_tags() if parse(tag.name) >= parse(start)]
-
+    return sorted([tag.name for tag in repo.get_tags() if parse(tag.name) >= parse(start)])
 
 if __name__ == "__main__":
     matrix = {}
@@ -19,11 +19,12 @@ if __name__ == "__main__":
         add_latest = sys.argv[1].lower() in ["--latest", "-l"]
     if environ.get("ACT"):
         # only run latests when running in ACT locally for testing
-        matrix["version"] = ["v1.18"]
+        matrix["version"] = micropython_versions(start="v1.17")[-1:]
     else:
         matrix["version"] = micropython_versions(start="v1.17")
         if add_latest:
             matrix["version"] += ["latest"]
     print(json.dumps(matrix))
 
-    print(f"::set-output name=versions::{json.dumps(matrix)}")
+    with open(os.getenv('GITHUB_OUTPUT'), 'a') as file:
+        file.write(f"versions={json.dumps(matrix)}")
