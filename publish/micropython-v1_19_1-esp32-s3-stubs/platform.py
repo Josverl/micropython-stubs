caaac3b4c641105ad7a6cedd872b542aaa153de7
Jos Verlinde <Jos.Verlinde@Microsoft.com>
2023-02-18 23:03:10 +0100

    Add esp32-S3

---
+++ publish/micropython-v1_19_1-esp32-s3-stubs/platform.py
@@ -0,0 +1,18 @@
+"""
+Module: 'platform' on micropython-v1.19.1-esp32
+"""
+# MCU: {'ver': 'v1.19.1', 'build': '', 'platform': 'esp32', 'port': 'esp32', 'machine': 'ESP32S3 module with ESP32S3', 'release': '1.19.1', 'nodename': 'esp32', 'name': 'micropython', 'family': 'micropython', 'sysname': 'esp32', 'version': '1.19.1'}
+# Stubber: 1.11.2
+from typing import Any
+
+
+def platform(*args, **kwargs) -> Any:
+    ...
+
+
+def python_compiler(*args, **kwargs) -> Any:
+    ...
+
+
+def libc_ver(*args, **kwargs) -> Any:
+    ...
---
+++ publish/micropython-v1_19_1-esp32-s3-stubs/utimeq.py
@@ -0,0 +1,20 @@
+"""
+Module: 'utimeq' on micropython-v1.19.1-esp32
+"""
+# MCU: {'ver': 'v1.19.1', 'build': '', 'platform': 'esp32', 'port': 'esp32', 'machine': 'ESP32S3 module with ESP32S3', 'release': '1.19.1', 'nodename': 'esp32', 'name': 'micropython', 'family': 'micropython', 'sysname': 'esp32', 'version': '1.19.1'}
+# Stubber: 1.11.2
+from typing import Any
+
+
+class utimeq:
+    def peektime(self, *args, **kwargs) -> Any:
+        ...
+
+    def push(self, *args, **kwargs) -> Any:
+        ...
+
+    def pop(self, *args, **kwargs) -> Any:
+        ...
+
+    def __init__(self, *argv, **kwargs) -> None:
+        ...
