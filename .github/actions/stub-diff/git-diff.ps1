1b5c7076c5fa61e02ec89056a1940eb182bfff12
Jos Verlinde <Jos.Verlinde@Microsoft.com>
2023-02-19 15:01:48 +0100

    Improve change detection to avoid too many updates containing only .json files

--- .github/actions/stub-diff/git-diff.ps1
+++ .github/actions/stub-diff/git-diff.ps1
@@ -1,18 +1,22 @@
 param(
-    $pattern = ".py"
+    $pattern = ".py",
+    $verbose = "true"
 )
+# take string and booleans as input
+try {
+    $verbose = [System.Convert]::ToBoolean($verbose) 
+}
+catch [FormatException] {
+    $verbose = $false
+}
 
-Write-host "::group:: git status"
+
+Write-host "::group:: git status -verbose:$verbose"
 # show current working directory
 Write-Host "pwd: $(pwd)" -ForegroundColor Green
 
-if ($env:ACT) {
-    # running in github act simulator 
-    $files = @("foo.py", "bar.py")
-}
-else {
-    $files = @(git status --porcelain | Where-Object { $_.Contains($pattern) })
-}
+
+$files = @(git status --porcelain | Where-Object { $_.Contains($pattern) })
 $count = $files.count
 
 $changed = "changed=$(if ($count -eq 0){"false"}else{"true"})"
@@ -22,8 +26,17 @@ $changed >> $env:GITHUB_OUTPUT
 
 # create a summary
 if ($count -gt 0) {
-    '### detected new stubs :rocket:' >> $env:GITHUB_STEP_SUMMARY
+    "### Detected $count new/changed stubs :rocket: :" >> $env:GITHUB_STEP_SUMMARY
     $files | ForEach-Object { "- $_" >> $env:GITHUB_STEP_SUMMARY }
+    if ($verbose) {
+        write-host "### detected $count new stubs:" 
+        $files | ForEach-Object { write-host "- $_" }
+    }
+}
+else {
+    if ($verbose) {
+        write-host '### no new stubs detected.'
+    }
 }
 
 write-host "::endgroup::"
