9264acdd3a49d9714262ddec64582ad5ee57925b
Josverl <Josverl@users.noreply.github.com>
2023-03-27 01:39:19 +0000

    Update frozen stubs for v1.18

---
+++ stubs/micropython-v1_18-frozen/esp32/LOLIN_S2_MINI/s2mini.pyi
@@ -0,0 +1,13 @@
+from _typeshed import Incomplete
+
+SPI_MOSI: Incomplete
+SPI_MISO: Incomplete
+SPI_CLK: Incomplete
+I2C_SDA: Incomplete
+I2C_SCL: Incomplete
+DAC1: Incomplete
+DAC2: Incomplete
+LED: Incomplete
+BUTTON: Incomplete
+led: Incomplete
+button: Incomplete
