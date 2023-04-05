3eea7803dbcfdd6691506aae30aea01b3a6a9510
Jos Verlinde <jos_verlinde@hotmail.com>
2023-04-05 16:07:19 +0200

    add hash
    Signed-off-by: Jos Verlinde <jos_verlinde@hotmail.com>

--- build_all_modules.py
+++ build_all_modules.py
@@ -5,6 +5,9 @@ this is build by running this script from the root of the micropython-stubs repo
   Therefore the package builds should be run before this script is run
 """
 
+
+import contextlib
+import hashlib
 import json
 import os
 import sys
@@ -20,6 +23,26 @@ except ModuleNotFoundError:
 
 ADD_STDLIB = False
 
+def partialhash(file:Path):
+    with open(file, "rb") as f:
+        # read by line and hash
+        md5 = hashlib.md5()
+        # skip the module docstring and initial comments
+        l = 0
+        for line in f:
+            # skip tripple quoated docstring 
+            if line.startswith(b'"""'):
+                l += 1  
+                for line in f:
+                    l += 1  
+                    if line.startswith(b'"""'):
+                        break
+            # skip comment
+            if line.startswith(b"#"):# and l < 10:
+                l += 1  
+                continue
+            md5.update(line)
+    return md5.hexdigest()
 
 def main(output_file="all_modules.json", input_dir="publish"):
     all_modules = []
@@ -45,12 +68,12 @@ def add_package(pkg_path:Path, all_modules, port="", board="", pkg_version=""):
         mpy_version = Version(pkg_version).base_version
         modules = pyproject["tool"]["poetry"]["packages"]
         familiy =""
-        try:
+
+        with contextlib.suppress(KeyError, IndexError):
             familiy = pyproject["tool"]["poetry"]["name"].split("-")[0]
             port = port or pyproject["tool"]["poetry"]["name"].split("-")[1]
-            board = board or pyproject["tool"]["poetry"]["name"].split("-")[2] 
-        except (KeyError, IndexError):
-            pass
+            board = board or pyproject["tool"]["poetry"]["name"].split("-")[2]
+
         if board == "stubs":
             board = "GENERIC"
         for mod in modules:
@@ -66,6 +89,7 @@ def add_package(pkg_path:Path, all_modules, port="", board="", pkg_version=""):
                             "board": board,
                             "package":  pkg_name, 
                             "pkg_version": pkg_version,
+                            "hash": partialhash(pkg_path.parent / mod["include"]),
                     }
             all_modules.append(row)
         if ADD_STDLIB:
