16f498351b1126b3b903bb81a718966e329d2fb0
Josverl <Josverl@users.noreply.github.com>
2023-04-27 01:34:27 +0000

    Update frozen stubs for latest

---
+++ stubs/micropython-v1_20-frozen/stm32/LEGO_HUB_NO7/appupdate.pyi
@@ -0,0 +1,11 @@
+from _typeshed import Incomplete
+
+_IOCTL_BLOCK_COUNT: Incomplete
+_IOCTL_BLOCK_SIZE: Incomplete
+_SPIFLASH_UPDATE_KEY_ADDR: Incomplete
+_SPIFLASH_UPDATE_KEY_VALUE: Incomplete
+_FILESYSTEM_ADDR: Incomplete
+flash: Incomplete
+_FILESYSTEM_LEN: Incomplete
+
+def update_app(filename) -> None: ...
