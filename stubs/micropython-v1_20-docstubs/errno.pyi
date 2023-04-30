1e08ca4b159e75d3473b25e4b1f7e6c8832b07da
Jos Verlinde <jos_verlinde@hotmail.com>
2023-04-30 16:11:03 +0200

    Add v1.20.0 docstubs
    Signed-off-by: Jos Verlinde <jos_verlinde@hotmail.com>

---
+++ stubs/micropython-v1_20-docstubs/errno.pyi
@@ -0,0 +1,47 @@
+"""
+system error codes. See: https://docs.micropython.org/en/v1.20/library/errno.html
+
+|see_cpython_module| :mod:`python:errno` https://docs.python.org/3/library/errno.html .
+
+This module provides access to symbolic error codes for `OSError` exception.
+A particular inventory of codes depends on :term:`MicroPython port`.
+"""
+
+# source version: v1_20
+# origin module:: repos/micropython/docs/library/errno.rst
+from typing import Any, Dict
+
+EEXIST: Any = ...
+"""\
+Error codes, based on ANSI C/POSIX standard. All error codes start with
+"E". As mentioned above, inventory of the codes depends on
+:term:`MicroPython port`. Errors are usually accessible as ``exc.errno``
+where ``exc`` is an instance of `OSError`. Usage example::
+
+try:
+os.mkdir("my_dir")
+except OSError as exc:
+if exc.errno == errno.EEXIST:
+print("Directory already exists")
+"""
+EAGAIN: Any = ...
+"""\
+Error codes, based on ANSI C/POSIX standard. All error codes start with
+"E". As mentioned above, inventory of the codes depends on
+:term:`MicroPython port`. Errors are usually accessible as ``exc.errno``
+where ``exc`` is an instance of `OSError`. Usage example::
+
+try:
+os.mkdir("my_dir")
+except OSError as exc:
+if exc.errno == errno.EEXIST:
+print("Directory already exists")
+"""
+errorcode: Dict
+"""\
+Dictionary mapping numeric error codes to strings with symbolic error
+code (see above)::
+
+>>> print(errno.errorcode[errno.EEXIST])
+EEXIST
+"""
---
+++ stubs/micropython-v1_20-docstubs/neopixel.pyi
@@ -0,0 +1,54 @@
+"""
+control of WS2812 / NeoPixel LEDs. See: https://docs.micropython.org/en/v1.20/library/neopixel.html
+
+This module provides a driver for WS2818 / NeoPixel LEDs.
+
+``Note:`` This module is only included by default on the ESP8266, ESP32 and RP2
+   ports. On STM32 / Pyboard and others, you can either install the
+   ``neopixel`` package using :term:`mip`, or you can download the module
+   directly from
+   <https://raw.githubusercontent.com/micropython/micropython-lib/master/micropython/drivers/led/neopixel/neopixel.py>`_
+   and copy it to the filesystem.
+"""
+
+# source version: v1_20
+# origin module:: repos/micropython/docs/library/neopixel.rst
+from typing import Tuple
+
+class NeoPixel:
+    """
+    Construct an NeoPixel object.  The parameters are:
+
+        - *pin* is a machine.Pin instance.
+        - *n* is the number of LEDs in the strip.
+        - *bpp* is 3 for RGB LEDs, and 4 for RGBW LEDs.
+        - *timing* is 0 for 400KHz, and 1 for 800kHz LEDs (most are 800kHz).
+    """
+
+    def __init__(self, pin, n, *, bpp=3, timing=1) -> None: ...
+    def fill(self, pixel) -> None:
+        """
+        Sets the value of all pixels to the specified *pixel* value (i.e. an
+        RGB/RGBW tuple).
+        """
+        ...
+    def __len__(self) -> int:
+        """
+        Returns the number of LEDs in the strip.
+        """
+        ...
+    def __setitem__(self, index, val) -> None:
+        """
+        Set the pixel at *index* to the value, which is an RGB/RGBW tuple.
+        """
+        ...
+    def __getitem__(self, index) -> Tuple:
+        """
+        Returns the pixel at *index* as an RGB/RGBW tuple.
+        """
+        ...
+    def write(self) -> None:
+        """
+        Writes the current pixel data to the strip.
+        """
+        ...
