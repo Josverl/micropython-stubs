c4e22bc674e1cd956252cf1a604661d6bb6e8fa2
Jos Verlinde <jos_verlinde@hotmail.com>
2023-05-05 20:40:13 +0200

    1.20.0 - rp2-Pico
    Signed-off-by: Jos Verlinde <jos_verlinde@hotmail.com>

---
+++ stubs/micropython-v1_20_0-rp2-PICO/cryptolib.py
@@ -0,0 +1,17 @@
+"""
+Module: 'cryptolib' on micropython-v1.20.0-rp2-PICO
+"""
+# MCU: OrderedDict({'family': 'micropython', 'version': '1.20.0', 'build': '', 'ver': 'v1.20.0', 'port': 'rp2', 'board': 'PICO', 'cpu': 'RP2040', 'mpy': 'v6.1', 'arch': 'armv6m'})
+# Stubber: v1.13.4
+from typing import Any
+
+
+class aes:
+    def encrypt(self, *args, **kwargs) -> Any:
+        ...
+
+    def decrypt(self, *args, **kwargs) -> Any:
+        ...
+
+    def __init__(self, *argv, **kwargs) -> None:
+        ...
---
+++ stubs/micropython-v1_20_0-rp2-PICO/select.py
@@ -0,0 +1,19 @@
+"""
+Module: 'select' on micropython-v1.20.0-rp2-PICO
+"""
+# MCU: OrderedDict({'family': 'micropython', 'version': '1.20.0', 'build': '', 'ver': 'v1.20.0', 'port': 'rp2', 'board': 'PICO', 'cpu': 'RP2040', 'mpy': 'v6.1', 'arch': 'armv6m'})
+# Stubber: v1.13.4
+from typing import Any
+
+POLLOUT = 4  # type: int
+POLLIN = 1  # type: int
+POLLHUP = 16  # type: int
+POLLERR = 8  # type: int
+
+
+def select(*args, **kwargs) -> Any:
+    ...
+
+
+def poll(*args, **kwargs) -> Any:
+    ...
