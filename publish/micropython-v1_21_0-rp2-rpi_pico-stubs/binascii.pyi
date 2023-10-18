398e8fea197efcae9ec45fc4868c1048e864c54f
Jos Verlinde <jos_verlinde@hotmail.com>
2023-10-18 22:31:54 +0200

    Publish v1.21.0 for rp2_RPI_PICO, rp2_RP_PICO_W and samd-SEEED_WIO_TERMINAL
    Signed-off-by: Jos Verlinde <jos_verlinde@hotmail.com>

---
+++ publish/micropython-v1_21_0-rp2-rpi_pico-stubs/binascii.pyi
@@ -0,0 +1,46 @@
+"""
+Binary/ASCII conversions.
+
+MicroPython module: https://docs.micropython.org/en/v1.21.0/library/binascii.html
+
+CPython module: :mod:`python:binascii` https://docs.python.org/3/library/binascii.html .
+
+This module implements conversions between binary data and various
+encodings of it in ASCII form (in both directions).
+"""
+from _typeshed import Incomplete, Incomplete as Incomplete
+from typing import Any, Optional
+
+def crc32(*args, **kwargs) -> Incomplete: ...
+def hexlify(data, sep: Optional[Any] = None) -> bytes:
+    """
+    Convert the bytes in the *data* object to a hexadecimal representation.
+    Returns a bytes object.
+
+    If the additional argument *sep* is supplied it is used as a separator
+    between hexadecimal values.
+    """
+    ...
+
+def unhexlify(data) -> bytes:
+    """
+    Convert hexadecimal data to binary representation. Returns bytes string.
+    (i.e. inverse of hexlify)
+    """
+    ...
+
+def b2a_base64(data, *, newline=True) -> bytes:
+    """
+    Encode binary data in base64 format, as in `RFC 3548
+    <https://tools.ietf.org/html/rfc3548.html>`_. Returns the encoded data
+    followed by a newline character if newline is true, as a bytes object.
+    """
+    ...
+
+def a2b_base64(data) -> bytes:
+    """
+    Decode base64-encoded data, ignoring invalid characters in the input.
+    Conforms to `RFC 2045 s.6.8 <https://tools.ietf.org/html/rfc2045#section-6.8>`_.
+    Returns a bytes object.
+    """
+    ...
