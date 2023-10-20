ad65af70898c1c5662a6dbcd5af8ae9b6ed08b94
Jos Verlinde <jos_verlinde@hotmail.com>
2023-10-20 10:29:05 +0200

    Published to PyPi  micropython-stm32-pybv11-stubs  1.21.0.post1
    Signed-off-by: Jos Verlinde <jos_verlinde@hotmail.com>

---
+++ stubs/micropython-v1_21_0-stm32-PYBV11-merged/network.pyi
@@ -0,0 +1,44 @@
+"""
+Network configuration.
+
+MicroPython module: https://docs.micropython.org/en/v1.21.0/library/network.html
+
+This module provides network drivers and routing configuration. To use this
+module, a MicroPython variant/build with network capabilities must be installed.
+Network drivers for specific hardware are available within this module and are
+used to configure hardware network interface(s). Network services provided
+by configured interfaces are then available for use via the :mod:`socket`
+module.
+
+For example::
+
+    # connect/ show IP config a specific network interface
+    # see below for examples of specific drivers
+    import network
+    import time
+    nic = network.Driver(...)
+    if not nic.isconnected():
+        nic.connect()
+        print("Waiting for connection...")
+        while not nic.isconnected():
+            time.sleep(1)
+    print(nic.ifconfig())
+
+    # now use socket as usual
+    import socket
+    addr = socket.getaddrinfo('micropython.org', 80)[0][-1]
+    s = socket.socket()
+    s.connect(addr)
+    s.send(b'GET / HTTP/1.1\r\nHost: micropython.org\r\n\r\n')
+    data = s.recv(1000)
+    s.close()
+"""
+from _typeshed import Incomplete, Incomplete as Incomplete
+from typing import Any, List, Optional, Tuple, Union
+
+STA_IF: int
+AP_IF: int
+
+def hostname(*args, **kwargs) -> Incomplete: ...
+def route(*args, **kwargs) -> Incomplete: ...
+def country(*args, **kwargs) -> Incomplete: ...
---
+++ stubs/micropython-v1_21_0-stm32-PYBV11/asyncio/funcs.pyi
@@ -0,0 +1,6 @@
+from _typeshed import Incomplete as Incomplete
+
+def wait_for_ms(*args, **kwargs) -> Incomplete: ...
+
+gather: Incomplete
+wait_for: Incomplete
