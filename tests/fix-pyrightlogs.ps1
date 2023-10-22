915576f126d9607c776ce449f816d8aa6b9c44df
Jos Verlinde <jos_verlinde@hotmail.com>
2023-10-22 23:48:12 +0200

    update GHA workflows
    Signed-off-by: Jos Verlinde <jos_verlinde@hotmail.com>

--- tests/fix-pyrightlogs.ps1
+++ tests/fix-pyrightlogs.ps1
@@ -9,17 +9,15 @@ foreach ( $file in (Get-ChildItem $Path -Filter "pyright*.log" )) {
     $content = $content -replace ' - (error|warning|information):', ': $1:' `
         -replace ' - (info):', ': information:' 
 
-    # promote some errors to warnings 
+    # Promote some errors to warnings 
     # $content = $content -replace 'warning: something (.*)', 'error: Something $1'  
-    
+
     # Demote some warnings to info 
     $content = $content -replace ': warning: "None" is not iterable', ': information: "None" is not iterable' `
         -replace ': warning: "([\w|_]+)" is unknown import symbol', ': information: "$1" is unknown import symbol' `
-    
 
-    # #remove some noise 
+    # Remove unwanted noise 
     $content = $content | Select-String -Pattern 'for type "None"' -NotMatch
 
     $content | Out-File $file
-
 }
