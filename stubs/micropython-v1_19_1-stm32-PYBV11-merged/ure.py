345135f2ae05bf0e9b74111c1bb087a1fee1a0b6
Jos Verlinde <jos_verlinde@hotmail.com>
2023-11-05 23:08:58 +0100

    Add stubs for PYBV11 board used in tests
    Signed-off-by: Jos Verlinde <jos_verlinde@hotmail.com>

---
+++ stubs/micropython-v1_19_1-stm32-PYBV11-merged/ure.py
@@ -0,0 +1,22 @@
+"""
+Module: 'ure' on micropython-v1.19.1-stm32
+"""
+# MCU: {'ver': 'v1.19.1', 'build': '', 'platform': 'stm32', 'port': 'stm32', 'machine': 'PYBv1.1 with STM32F405RG', 'release': '1.19.1', 'nodename': 'pyboard', 'name': 'micropython', 'family': 'micropython', 'sysname': 'pyboard', 'version': '1.19.1'}
+# Stubber: 1.9.11
+from typing import Any
+
+
+def sub(*args, **kwargs) -> Any:
+    ...
+
+
+def search(*args, **kwargs) -> Any:
+    ...
+
+
+def match(*args, **kwargs) -> Any:
+    ...
+
+
+def compile(*args, **kwargs) -> Any:
+    ...
