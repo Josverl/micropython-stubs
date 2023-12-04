545254ec34a752c6df80906c4678dbd823abe7a3
Jos Verlinde <jos_verlinde@hotmail.com>
2023-12-04 08:21:47 +0100

    chore: Update-stubs.ps1 script to conditionally update latest version
    Signed-off-by: Jos Verlinde <jos_verlinde@hotmail.com>

--- update-stubs.ps1
+++ update-stubs.ps1
@@ -3,12 +3,15 @@
 param(
     [string]$port = "auto",
     [string]$board = "auto",
-    [string[]]$versions = @("latest","v1.21.0", "v1.20.0", "v1.19.1" )
+    [string[]]$versions = @("latest","v1.21.0", "v1.20.0", "v1.19.1" ),
+    [switch]$force
 )
 # update the stubs (local ) do not push updates
 foreach ($version in $versions) {
-    stubber get-docstubs --version $version
-    stubber get-frozen --version $version
+    if ($version -eq "latest" -or $force ) {
+        stubber get-docstubs --version $version
+        stubber get-frozen --version $version
+    }
     stubber merge --version $version --port $port --board $board
     stubber build --version $version --port $port --board $board
 }
