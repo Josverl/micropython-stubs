86765a27ed36915f28a7702f91d476391faef926
Jos Verlinde <jos.verlinde@microsoft.com>
2024-02-17 00:28:13 +0100

    add samd v1.21.1
    Signed-off-by: Jos Verlinde <jos.verlinde@microsoft.com>

--- stubs/micropython-v1_22_1-samd-SEEED_WIO_TERMINAL/random.pyi
+++ stubs/micropython-v1_22_1-samd-SEEED_WIO_TERMINAL/random.pyi
@@ -2,7 +2,7 @@
 Module: 'random' on micropython-v1.22.1-samd-SEEED_WIO_TERMINAL
 """
 # MCU: {'build': '', 'ver': '1.22.1', 'version': '1.22.1', 'port': 'samd', 'board': 'SEEED_WIO_TERMINAL', 'mpy': 'v6.2', 'family': 'micropython', 'cpu': 'SAMD51P19A', 'arch': 'armv7emsp'}
-# Stubber: v1.16.3
+# Stubber: v1.17.1
 from __future__ import annotations
 from _typeshed import Incomplete
 
