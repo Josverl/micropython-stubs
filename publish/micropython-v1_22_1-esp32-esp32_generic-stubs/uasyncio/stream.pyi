3a80f6a0f464e8593248a3b3815df01d721e82f0
Josverl <Josverl@users.noreply.github.com>
2024-02-27 09:16:39 +0000

    update stubs

---
+++ publish/micropython-v1_22_1-esp32-esp32_generic-stubs/uasyncio/stream.pyi
@@ -0,0 +1,67 @@
+"""
+Module: 'uasyncio.stream' on micropython-v1.23.0-preview-esp32-ESP32_GENERIC
+"""
+# MCU: {'family': 'micropython', 'version': '1.23.0-preview', 'build': 'preview.6.g3d0b6276f', 'ver': '1.23.0-preview-preview.6.g3d0b6276f', 'port': 'esp32', 'board': 'ESP32_GENERIC', 'cpu': 'ESP32', 'mpy': 'v6.2', 'arch': 'xtensawin'}
+# Stubber: v1.16.3
+from __future__ import annotations
+from typing import Generator
+from _typeshed import Incomplete
+
+stream_awrite: Generator  ## = <generator>
+open_connection: Generator  ## = <generator>
+start_server: Generator  ## = <generator>
+
+class StreamWriter:
+    def write(self, *args, **kwargs) -> Incomplete: ...
+    def get_extra_info(self, *args, **kwargs) -> Incomplete: ...
+    def close(self, *args, **kwargs) -> Incomplete: ...
+
+    awritestr: Generator  ## = <generator>
+    wait_closed: Generator  ## = <generator>
+    drain: Generator  ## = <generator>
+    readexactly: Generator  ## = <generator>
+    readinto: Generator  ## = <generator>
+    read: Generator  ## = <generator>
+    awrite: Generator  ## = <generator>
+    readline: Generator  ## = <generator>
+    aclose: Generator  ## = <generator>
+    def __init__(self, *argv, **kwargs) -> None: ...
+
+class Server:
+    def close(self, *args, **kwargs) -> Incomplete: ...
+
+    wait_closed: Generator  ## = <generator>
+    _serve: Generator  ## = <generator>
+    def __init__(self, *argv, **kwargs) -> None: ...
+
+class Stream:
+    def write(self, *args, **kwargs) -> Incomplete: ...
+    def get_extra_info(self, *args, **kwargs) -> Incomplete: ...
+    def close(self, *args, **kwargs) -> Incomplete: ...
+
+    awritestr: Generator  ## = <generator>
+    wait_closed: Generator  ## = <generator>
+    drain: Generator  ## = <generator>
+    readexactly: Generator  ## = <generator>
+    readinto: Generator  ## = <generator>
+    read: Generator  ## = <generator>
+    awrite: Generator  ## = <generator>
+    readline: Generator  ## = <generator>
+    aclose: Generator  ## = <generator>
+    def __init__(self, *argv, **kwargs) -> None: ...
+
+class StreamReader:
+    def write(self, *args, **kwargs) -> Incomplete: ...
+    def get_extra_info(self, *args, **kwargs) -> Incomplete: ...
+    def close(self, *args, **kwargs) -> Incomplete: ...
+
+    awritestr: Generator  ## = <generator>
+    wait_closed: Generator  ## = <generator>
+    drain: Generator  ## = <generator>
+    readexactly: Generator  ## = <generator>
+    readinto: Generator  ## = <generator>
+    read: Generator  ## = <generator>
+    awrite: Generator  ## = <generator>
+    readline: Generator  ## = <generator>
+    aclose: Generator  ## = <generator>
+    def __init__(self, *argv, **kwargs) -> None: ...
--- publish/micropython-v1_22_1-samd-stubs/binascii.pyi
+++ publish/micropython-v1_22_1-samd-stubs/binascii.pyi
@@ -12,7 +12,7 @@ encodings of it in ASCII form (in both directions).
 Module: 'binascii' on micropython-v1.22.1-samd-SEEED_WIO_TERMINAL
 """
 # MCU: {'build': '', 'ver': '1.22.1', 'version': '1.22.1', 'port': 'samd', 'board': 'SEEED_WIO_TERMINAL', 'mpy': 'v6.2', 'family': 'micropython', 'cpu': 'SAMD51P19A', 'arch': 'armv7emsp'}
-# Stubber: v1.16.3
+# Stubber: v1.17.1
 from __future__ import annotations
 from _typeshed import Incomplete
 from typing import Any, Optional
