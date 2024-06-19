68bce3f57fa60e1567e22600c75dab33ff992be8
Jos Verlinde <jos_verlinde@hotmail.com>
2024-06-20 00:20:48 +0200

    publish micropython-stubs v1.23.0

---
+++ publish/micropython-v1_23_0-samd-stubs/uasyncio/funcs.pyi
@@ -0,0 +1,20 @@
+"""
+Module: 'uasyncio.funcs' on micropython-v1.23.0-samd-SEEED_WIO_TERMINAL
+"""
+
+# MCU: {'version': '1.23.0', 'mpy': 'v6.3', 'port': 'samd', 'board': 'SEEED_WIO_TERMINAL', 'family': 'micropython', 'build': '', 'arch': 'armv7emsp', 'ver': '1.23.0', 'cpu': 'SAMD51P19A'}
+# Stubber: v1.20.0
+from __future__ import annotations
+from typing import Generator
+from _typeshed import Incomplete
+
+def wait_for_ms(*args, **kwargs) -> Incomplete: ...
+
+gather: Generator  ## = <generator>
+wait_for: Generator  ## = <generator>
+
+class _Remove:
+    def remove(self, *args, **kwargs) -> Incomplete: ...
+    def __init__(self, *argv, **kwargs) -> None: ...
+
+_run: Generator  ## = <generator>
