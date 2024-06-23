0dce31f0221c754e201479a26c5c7bec201152ac
Jos Verlinde <jos_verlinde@hotmail.com>
2024-06-23 18:20:12 +0200

    actions: Update listversions to return the N- most recent Major.Minors

--- .github/workflows/list_versions.py
+++ .github/workflows/list_versions.py
@@ -11,7 +11,7 @@ import os
 import sys
 
 from github import Github
-from packaging.version import parse
+from packaging.version import parse, Version
 
 
 def micropython_versions(start="v1.10"):
@@ -25,18 +25,25 @@ def micropython_versions(start="v1.10"):
         tags = ["v1.19.1"]
     return tags
 
+def major_minor(versions):
+    """create a list of the most recent version for each major.minor"""
+    mm_groups = {}
+    for v in versions:
+        major_minor = f"{Version(v).major}.{Version(v).minor}"
+        if major_minor not in mm_groups:
+            mm_groups[major_minor] = [v]
+        else:
+            mm_groups[major_minor].append(v)
+    return [max(v) for v in mm_groups.values()]
 
 def main():
-    matrix = {}
     # only run latests when running in ACT locally for testing
-    if os.environ.get("ACT"):
-        matrix["version"] = micropython_versions(start="v1.20")[:1] # only latest
-    else:
-       matrix["version"] = micropython_versions(start="v1.20")[:3] # last three
+    max_versions = 1 if os.environ.get("ACT") else 3
 
-    add_latest = False
+    matrix = {}
+    matrix["version"] = major_minor(micropython_versions(start="v1.20"))[:max_versions] # last three
+
     if len(sys.argv) > 1 and (sys.argv[1].lower() in ["--latest", "-l"]):
-        # print("Adding latest")
         matrix["version"].insert(0, "latest")
 
     # GITHUB_OUTPUT is set by github actions
