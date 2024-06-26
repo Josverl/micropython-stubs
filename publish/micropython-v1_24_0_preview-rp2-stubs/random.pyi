00bb52b09dffd25e0909e82f245bc5a1d89972b1
Josverl <Josverl@users.noreply.github.com>
2024-06-26 01:40:51 +0000

    update stubs

--- publish/micropython-v1_24_0_preview-rp2-stubs/random.pyi
+++ publish/micropython-v1_24_0_preview-rp2-stubs/random.pyi
@@ -30,7 +30,7 @@ CPython module: :mod:`python:random` https://docs.python.org/3/library/random.ht
 Module: 'random' on micropython-v1.24.0-preview-rp2-RPI_PICO
 """
 
-# MCU: {'build': 'preview.62.g908ab1cec', 'ver': '1.24.0-preview-preview.62.g908ab1cec', 'version': '1.24.0-preview', 'port': 'rp2', 'board': 'RPI_PICO', 'mpy': 'v6.3', 'family': 'micropython', 'cpu': 'RP2040', 'arch': 'armv6m'}
+# MCU: {'build': 'preview.63.gcfa55b4ca', 'ver': '1.24.0-preview-preview.63.gcfa55b4ca', 'version': '1.24.0-preview', 'port': 'rp2', 'board': 'RPI_PICO', 'mpy': 'v6.3', 'family': 'micropython', 'cpu': 'RP2040', 'arch': 'armv6m'}
 # Stubber: v1.20.0
 from __future__ import annotations
 from _typeshed import Incomplete
--- publish/micropython-v1_24_0_preview-samd-stubs/sys.pyi
+++ publish/micropython-v1_24_0_preview-samd-stubs/sys.pyi
@@ -9,7 +9,7 @@ CPython module: :mod:`python:sys` https://docs.python.org/3/library/sys.html .
 Module: 'sys' on micropython-v1.24.0-preview-samd-SEEED_WIO_TERMINAL
 """
 
-# MCU: {'version': '1.24.0-preview', 'mpy': 'v6.3', 'port': 'samd', 'board': 'SEEED_WIO_TERMINAL', 'family': 'micropython', 'build': '62', 'arch': 'armv7emsp', 'ver': '1.24.0-preview-62', 'cpu': 'SAMD51P19A'}
+# MCU: {'version': '1.24.0-preview', 'mpy': 'v6.3', 'port': 'samd', 'board': 'SEEED_WIO_TERMINAL', 'family': 'micropython', 'build': '63', 'arch': 'armv7emsp', 'ver': '1.24.0-preview-63', 'cpu': 'SAMD51P19A'}
 # Stubber: v1.20.0
 from __future__ import annotations
 from _typeshed import Incomplete
@@ -18,7 +18,7 @@ from typing import Dict, List, Tuple
 platform: str = "samd"
 version_info: tuple = ()
 path: list = []
-version: str = "3.4.0; MicroPython v1.24.0-preview.62.g908ab1cec on 2024-06-24"
+version: str = "3.4.0; MicroPython v1.24.0-preview.63.gcfa55b4ca on 2024-06-25"
 ps1: str = ">>> "
 ps2: str = "... "
 byteorder: str = "little"
