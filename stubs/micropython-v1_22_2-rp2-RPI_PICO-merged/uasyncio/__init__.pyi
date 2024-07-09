a6f71cb792b6143a52c6c4d2b0ed12c92492ea6c
Josverl <Josverl@users.noreply.github.com>
2024-07-10 01:40:16 +0200

    Update boardstubs for v1.22.2

--- stubs/micropython-v1_22_2-rp2-RPI_PICO-merged/uasyncio/__init__.pyi
+++ stubs/micropython-v1_22_2-rp2-RPI_PICO-merged/uasyncio/__init__.pyi
@@ -38,7 +38,7 @@ Module: 'uasyncio.__init__' on micropython-v1.22.2-rp2-RPI_PICO
 """
 
 # MCU: {'build': '', 'ver': '1.22.2', 'version': '1.22.2', 'port': 'rp2', 'board': 'RPI_PICO', 'mpy': 'v6.2', 'family': 'micropython', 'cpu': 'RP2040', 'arch': 'armv6m'}
-# Stubber: v1.20.0
+# Stubber: v1.23.0
 from __future__ import annotations
 from typing import Any, Coroutine, List, Tuple, Generator
 from _typeshed import Incomplete
--- stubs/micropython-v1_22_2-stm32-PYBV11/usys.pyi
+++ stubs/micropython-v1_22_2-stm32-PYBV11/usys.pyi
@@ -3,7 +3,7 @@ Module: 'usys' on micropython-v1.22.2-stm32-PYBV11
 """
 
 # MCU: {'version': '1.22.2', 'mpy': 'v6.2', 'port': 'stm32', 'board': 'PYBV11', 'family': 'micropython', 'build': '', 'arch': 'armv7emsp', 'ver': '1.22.2', 'cpu': 'STM32F405RG'}
-# Stubber: v1.20.0
+# Stubber: v1.23.0
 from __future__ import annotations
 from _typeshed import Incomplete
 
