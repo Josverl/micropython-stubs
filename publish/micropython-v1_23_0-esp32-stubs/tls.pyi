a135e53bf519a7946af5c19bf602aa97ad65c67d
Josverl <Josverl@users.noreply.github.com>
2024-07-23 01:41:56 +0000

    update stubs

---
+++ publish/micropython-v1_23_0-esp32-stubs/tls.pyi
@@ -0,0 +1,23 @@
+"""
+Module: 'tls' on micropython-v1.23.0-esp32-ESP32_GENERIC
+"""
+
+# MCU: {'version': '1.23.0', 'mpy': 'v6.3', 'port': 'esp32', 'board': 'ESP32_GENERIC', 'family': 'micropython', 'build': '', 'arch': 'xtensawin', 'ver': '1.23.0', 'cpu': 'ESP32'}
+# Stubber: v1.23.0
+from __future__ import annotations
+from _typeshed import Incomplete
+
+MBEDTLS_VERSION: str = "mbed TLS 3.4.1"
+PROTOCOL_TLS_SERVER: int = 1
+PROTOCOL_TLS_CLIENT: int = 0
+CERT_NONE: int = 0
+CERT_REQUIRED: int = 2
+CERT_OPTIONAL: int = 1
+
+class SSLContext:
+    def load_verify_locations(self, *args, **kwargs) -> Incomplete: ...
+    def set_ciphers(self, *args, **kwargs) -> Incomplete: ...
+    def wrap_socket(self, *args, **kwargs) -> Incomplete: ...
+    def load_cert_chain(self, *args, **kwargs) -> Incomplete: ...
+    def get_ciphers(self, *args, **kwargs) -> Incomplete: ...
+    def __init__(self, *argv, **kwargs) -> None: ...
--- publish/micropython-v1_23_0-esp8266-stubs/uarray.pyi
+++ publish/micropython-v1_23_0-esp8266-stubs/uarray.pyi
@@ -14,7 +14,7 @@ Module: 'uarray' on micropython-v1.23.0-esp8266-ESP8266_GENERIC
 """
 
 # MCU: {'version': '1.23.0', 'mpy': 'v6.3', 'port': 'esp8266', 'board': 'ESP8266_GENERIC', 'family': 'micropython', 'build': '', 'arch': 'xtensa', 'ver': '1.23.0', 'cpu': 'ESP8266'}
-# Stubber: v1.20.0
+# Stubber: v1.23.0
 from __future__ import annotations
 from _typeshed import Incomplete
 from typing import Any, List, Optional
