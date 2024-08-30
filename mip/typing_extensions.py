5e7b40f32d6a4b20b00ba66946f086cf68bdb26f
Jos Verlinde <Jos.Verlinde@microsoft.com>
2024-08-31 01:46:52 +0200

    Update typing* and abc module to improve runtime for typed files.
    related: https://github.com/micropython/micropython-lib/pull/584
    Signed-off-by: Jos Verlinde <Jos.Verlinde@microsoft.com>

--- mip/typing_extensions.py
+++ mip/typing_extensions.py
@@ -1,3 +1,9 @@
+"""
+This module provides runtime support for type hints.	
+based on : https://github.com/micropython/micropython-lib/pull/584
+"""
+
+
 def cast(type, val):
     return val
 
@@ -18,137 +24,28 @@ def overload(func):
     return None
 
 
+def NewType(name, type):
+    return type
+
+
 class _AnyCall:
+    """A class to ignore type hints in code."""
+
     def __init__(*args, **kwargs):
         pass
 
     def __call__(*args, **kwargs):
         pass
 
-
-_anyCall = _AnyCall()
-
-
-class _SubscriptableType:
     def __getitem__(self, arg):
-        return _anyCall
+        return _any_call
 
 
-_Subscriptable = _SubscriptableType()
-
-
-def TypeVar(type, *types):
-    return None
-
-
-def NewType(name, type):
-    return type
-
-
-class Any:
-    pass
-
-
-class BinaryIO:
-    pass
-
-
-class ClassVar:
-    pass
-
-
-class Final:
-    pass
-
-
-class Hashable:
-    pass
-
-
-class IO:
-    pass
-
-
-class NoReturn:
-    pass
-
-
-class Sized:
-    pass
-
-
-class SupportsInt:
-    pass
-
-
-class SupportsFloat:
-    pass
-
-
-class SupportsComplex:
-    pass
-
-
-class SupportsBytes:
-    pass
-
-
-class SupportsIndex:
-    pass
-
-
-class SupportsAbs:
-    pass
-
-
-class SupportsRound:
-    pass
-
-
-class TextIO:
-    pass
-
-
-AnyStr = str
-Text = str
-Pattern = str
-Match = str
-TypedDict = dict
-
-AbstractSet = _Subscriptable
-AsyncContextManager = _Subscriptable
-AsyncGenerator = _Subscriptable
-AsyncIterable = _Subscriptable
-AsyncIterator = _Subscriptable
-Awaitable = _Subscriptable
-Callable = _Subscriptable
-ChainMap = _Subscriptable
-Collection = _Subscriptable
-Container = _Subscriptable
-ContextManager = _Subscriptable
-Coroutine = _Subscriptable
-Counter = _Subscriptable
-DefaultDict = _Subscriptable
-Deque = _Subscriptable
-Dict = _Subscriptable
-FrozenSet = _Subscriptable
-Generator = _Subscriptable
-Generic = _Subscriptable
-Iterable = _Subscriptable
-Iterator = _Subscriptable
-List = _Subscriptable
-Literal = _Subscriptable
-Mapping = _Subscriptable
-MutableMapping = _Subscriptable
-MutableSequence = _Subscriptable
-MutableSet = _Subscriptable
-NamedTuple = _Subscriptable
-Optional = _Subscriptable
-OrderedDict = _Subscriptable
-Sequence = _Subscriptable
-Set = _Subscriptable
-Tuple = _Subscriptable
-Type = _Subscriptable
-Union = _Subscriptable
+_any_call = _AnyCall()
 
 TYPE_CHECKING = False
+
+
+# ref: https://github.com/micropython/micropython-lib/pull/584#issuecomment-2317690854
+def __getattr__(attr):
+    return _any_call
