618abbf08d772a2c38aedd5d0db54e2180cbed78
Josverl <Josverl@users.noreply.github.com>
2024-10-22 22:02:43 +0000

    Update docstubs stubs for preview

--- stubs/micropython-v1_24_0_preview-docstubs/machine.pyi
+++ stubs/micropython-v1_24_0_preview-docstubs/machine.pyi
@@ -732,7 +732,7 @@ class UART:
 
         .. note::
 
-            For the rp2, esp8266 and nrf ports the call returns while the last byte is sent.
+            For the esp8266 and nrf ports the call returns while the last byte is sent.
             If required, a one character wait time has to be added in the calling script.
 
         Availability: rp2, esp32, esp8266, mimxrt, cc3200, stm32, nrf ports, renesas-ra
@@ -746,7 +746,7 @@ class UART:
 
         .. note::
 
-            For the rp2, esp8266 and nrf ports the call may return ``True`` even if the last byte
+            For the esp8266 and nrf ports the call may return ``True`` even if the last byte
             of a transfer is still being sent. If required, a one character wait time has to be
             added in the calling script.
 
