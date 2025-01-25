20bf414e20fff679c06a6f2efbf3a25c77bee5df
Jos Verlinde <Jos.Verlinde@microsoft.com>
2025-01-26 00:26:13 +0100

    tests: Update stub installation logic and remove obsolete PowerShell script
    Signed-off-by: Jos Verlinde <Jos.Verlinde@microsoft.com>

--- tests/quality_tests/conftest.py
+++ tests/quality_tests/conftest.py
@@ -147,7 +147,7 @@ def install_stubs(portboard, version, stub_source, pytestconfig, tsc_path: Path)
     elif version == "latest":
         # use the latest release version
         version = get_stable_mp_version()
-        
+
     flatversion = flat_version(version)
     # clean up prior install to avoid stale files
     if tsc_path.exists():
@@ -168,10 +168,11 @@ def install_stubs(portboard, version, stub_source, pytestconfig, tsc_path: Path)
         else:
             foldername = f"micropython-{flatversion}-{portboard}-stubs"
         # stubsource = pytestconfig.inipath.parent / f"repos/micropython-stubs/publish/{foldername}"
-        stubsource = pytestconfig.inipath.parent / f"publish/{foldername}"
-        if not stubsource.exists():
-            pytest.skip(f"Could not find stubs for {portboard} {version} at {stubsource}")
-        cmd = f"uv pip install {stubsource} --pre --target {tsc_path} --no-cache"
+        stdlib_source = pytestconfig.inipath.parent / f"publish/{foldername}"
+        stubs_source = pytestconfig.inipath.parent / f"publish/micropython-stdlib-stubs"
+        if not stubs_source.exists():
+            pytest.skip(f"Could not find stubs for {portboard} {version} at {stubs_source}")
+        cmd = f"uv pip install {stdlib_source} {stubs_source} --pre --target {tsc_path} --no-cache"
 
     try:
         subprocess.run(cmd, shell=True, check=True, capture_output=True, text=True)
