aa1c11d8599914e95f3a4abfe10339c19f1aa3a2
Jos Verlinde <Jos.Verlinde@microsoft.com>
2025-02-09 05:56:39 -0800

    test: add micropython.native tests
    Signed-off-by: Jos Verlinde <Jos.Verlinde@microsoft.com>

--- tests/quality_tests/feat_micropython/check_micropython/check_native.py
+++ tests/quality_tests/feat_micropython/check_micropython/check_native.py
@@ -1,7 +1,17 @@
 import micropython
 
+
 @micropython.native
 def foo(self, arg):
-    buf = self.linebuf # Cached object
+    buf = self.linebuf  # Cached object
     # code
 
+
+@micropython.native
+def bar(self, arg) -> int:
+    # code
+    return 1
+
+
+reveal_type(foo) 
+reveal_type(bar) 
