ca1714a43de5384f8d0ecc62d08ab1339258bbd4
Josverl <Josverl@users.noreply.github.com>
2025-02-14 22:03:12 +0000

    Update docstubs stubs for preview

--- stubs/micropython-v1_25_0_preview-docstubs/ssl/__init__.pyi
+++ stubs/micropython-v1_25_0_preview-docstubs/ssl/__init__.pyi
@@ -14,18 +14,20 @@ facilities for network sockets, both client-side and server-side.
 # origin module:: repos/micropython/docs/library/ssl.rst
 from __future__ import annotations
 from _typeshed import Incomplete
-from typing import List
+from typing import IO, Any, Callable, Coroutine, Dict, Generator, Iterator, List, NoReturn, Optional, Tuple, Union, NamedTuple
 from typing_extensions import TypeVar, TypeAlias, Awaitable
 from typing_extensions import TypeAlias
 from _mpy_shed import StrOrBytesPath
-from socket import socket
-
-SSLSocket: TypeAlias = Incomplete
+SSLSocket : TypeAlias = Incomplete
 SSLError: Incomplete
 """This exception does NOT exist. Instead its base class, OSError, is used."""
 PROTOCOL_TLS_CLIENT: Incomplete
 """Supported values for the *protocol* parameter."""
 PROTOCOL_TLS_SERVER: Incomplete
+"""Supported values for the *protocol* parameter."""
+PROTOCOL_DTLS_CLIENT (when DTLS support is enabled): Incomplete
+"""Supported values for the *protocol* parameter."""
+PROTOCOL_DTLS_SERVER (when DTLS support is enabled): Incomplete
 """Supported values for the *protocol* parameter."""
 CERT_NONE: Incomplete
 """\
@@ -42,107 +44,84 @@ CERT_REQUIRED: Incomplete
 Supported values for *cert_reqs* parameter, and the :attr:`SSLContext.verify_mode`
 attribute.
 """
-
-class SSLContext:
+class SSLContext():
     """
-    Create a new SSLContext instance.  The *protocol* argument must be one of the ``PROTOCOL_*``
-    constants.
+        Create a new SSLContext instance.  The *protocol* argument must be one of the ``PROTOCOL_*``
+        constants.
     """
-
-    def __init__(self, *argv, **kwargs) -> None: ...
+    def __init__(self, protocol, /) -> None:
+        ...
     def load_cert_chain(self, certfile, keyfile) -> None:
         """
-        Load a private key and the corresponding certificate.  The *certfile* is a string
-        with the file path of the certificate.  The *keyfile* is a string with the file path
-        of the private key.
-
-        Admonition:Difference to CPython
-           :class: attention
-
-           MicroPython extension: *certfile* and *keyfile* can be bytes objects instead of
-           strings, in which case they are interpreted as the actual certificate/key data.
+           Load a private key and the corresponding certificate.  The *certfile* is a string
+           with the file path of the certificate.  The *keyfile* is a string with the file path
+           of the private key.
+        
+           Admonition:Difference to CPython
+              :class: attention
+        
+              MicroPython extension: *certfile* and *keyfile* can be bytes objects instead of
+              strings, in which case they are interpreted as the actual certificate/key data.
         """
         ...
-
-    def load_verify_locations(self, cafile=None, cadata: bytes = None) -> None:
+    def load_verify_locations(self, cafile=None, cadata=None) -> None:
         """
-        Load the CA certificate chain that will validate the peer's certificate.
-        *cafile* is the file path of the CA certificates.  *cadata* is a bytes object
-        containing the CA certificates.  Only one of these arguments should be provided.
+           Load the CA certificate chain that will validate the peer's certificate.
+           *cafile* is the file path of the CA certificates.  *cadata* is a bytes object
+           containing the CA certificates.  Only one of these arguments should be provided.
         """
         ...
-
     def get_ciphers(self) -> List[str]:
         """
-        Get a list of enabled ciphers, returned as a list of strings.
+           Get a list of enabled ciphers, returned as a list of strings.
         """
         ...
-
     def set_ciphers(self, ciphers) -> None:
         """
-        Set the available ciphers for sockets created with this context.  *ciphers* should be
-        a list of strings in the `IANA cipher suite format <https://wiki.mozilla.org/Security/Cipher_Suites>`_ .
+           Set the available ciphers for sockets created with this context.  *ciphers* should be
+           a list of strings in the `IANA cipher suite format <https://wiki.mozilla.org/Security/Cipher_Suites>`_ .
         """
         ...
-
-    def wrap_socket(
-        self,
-        sock: socket,
-        *,
-        server_side: bool = False,
-        do_handshake_on_connect: bool = True,
-        server_hostname: str | None = None,
-    ) -> SSLSocket:
+    def wrap_socket(self, sock, *, server_side=False, do_handshake_on_connect=True, server_hostname=None) -> Incomplete:
         """
-        Takes a `stream` *sock* (usually socket.socket instance of ``SOCK_STREAM`` type),
-        and returns an instance of ssl.SSLSocket, wrapping the underlying stream.
-        The returned object has the usual `stream` interface methods like
-        ``read()``, ``write()``, etc.
-
-        - *server_side* selects whether the wrapped socket is on the server or client side.
-          A server-side SSL socket should be created from a normal socket returned from
-          :meth:`~socket.socket.accept()` on a non-SSL listening server socket.
-
-        - *do_handshake_on_connect* determines whether the handshake is done as part of the ``wrap_socket``
-          or whether it is deferred to be done as part of the initial reads or writes
-          For blocking sockets doing the handshake immediately is standard. For non-blocking
-          sockets (i.e. when the *sock* passed into ``wrap_socket`` is in non-blocking mode)
-          the handshake should generally be deferred because otherwise ``wrap_socket`` blocks
-          until it completes. Note that in AXTLS the handshake can be deferred until the first
-          read or write but it then blocks until completion.
-
-        - *server_hostname* is for use as a client, and sets the hostname to check against the received
-          server certificate.  It also sets the name for Server Name Indication (SNI), allowing the server
-          to present the proper certificate.
+           Takes a `stream` *sock* (usually socket.socket instance of ``SOCK_STREAM`` type),
+           and returns an instance of ssl.SSLSocket, wrapping the underlying stream.
+           The returned object has the usual `stream` interface methods like
+           ``read()``, ``write()``, etc.
+        
+           - *server_side* selects whether the wrapped socket is on the server or client side.
+             A server-side SSL socket should be created from a normal socket returned from
+             :meth:`~socket.socket.accept()` on a non-SSL listening server socket.
+        
+           - *do_handshake_on_connect* determines whether the handshake is done as part of the ``wrap_socket``
+             or whether it is deferred to be done as part of the initial reads or writes
+             For blocking sockets doing the handshake immediately is standard. For non-blocking
+             sockets (i.e. when the *sock* passed into ``wrap_socket`` is in non-blocking mode)
+             the handshake should generally be deferred because otherwise ``wrap_socket`` blocks
+             until it completes. Note that in AXTLS the handshake can be deferred until the first
+             read or write but it then blocks until completion.
+        
+           - *server_hostname* is for use as a client, and sets the hostname to check against the received
+             server certificate.  It also sets the name for Server Name Indication (SNI), allowing the server
+             to present the proper certificate.
         """
         ...
-
-def wrap_socket(
-    sock: socket,
-    *,
-    server_side: bool = False,
-    key: Incomplete = None,
-    cert: Incomplete = None,
-    cert_reqs: int = 0,
-    cadata: bytes | None = None,
-    server_hostname: str | None = None,
-    do_handshake: bool = True,
-) -> SSLSocket:
+def wrap_socket(sock, server_side=False, key=None, cert=None, cert_reqs=None, cadata=None, server_hostname=None, do_handshake=True) -> SSLSocket:
     """
-     Wrap the given *sock* and return a new wrapped-socket object.  The implementation
-     of this function is to first create an `SSLContext` and then call the `SSLContext.wrap_socket`
-     method on that context object.  The arguments *sock*, *server_side* and *server_hostname* are
-     passed through unchanged to the method call.  The argument *do_handshake* is passed through as
-     *do_handshake_on_connect*.  The remaining arguments have the following behaviour:
-
-    - *cert_reqs* determines whether the peer (server or client) must present a valid certificate.
-      Note that for mbedtls based ports, ``ssl.CERT_NONE`` and ``ssl.CERT_OPTIONAL`` will not
-      validate any certificate, only ``ssl.CERT_REQUIRED`` will.
-
-    - *cadata* is a bytes object containing the CA certificate chain (in DER format) that will
-      validate the peer's certificate.  Currently only a single DER-encoded certificate is supported.
-
-    Depending on the underlying module implementation in a particular
-    :term:`MicroPython port`, some or all keyword arguments above may be not supported.
+        Wrap the given *sock* and return a new wrapped-socket object.  The implementation
+        of this function is to first create an `SSLContext` and then call the `SSLContext.wrap_socket`
+        method on that context object.  The arguments *sock*, *server_side* and *server_hostname* are
+        passed through unchanged to the method call.  The argument *do_handshake* is passed through as
+        *do_handshake_on_connect*.  The remaining arguments have the following behaviour:
+    
+       - *cert_reqs* determines whether the peer (server or client) must present a valid certificate.
+         Note that for mbedtls based ports, ``ssl.CERT_NONE`` and ``ssl.CERT_OPTIONAL`` will not
+         validate any certificate, only ``ssl.CERT_REQUIRED`` will.
+    
+       - *cadata* is a bytes object containing the CA certificate chain (in DER format) that will
+         validate the peer's certificate.  Currently only a single DER-encoded certificate is supported.
+    
+       Depending on the underlying module implementation in a particular
+       :term:`MicroPython port`, some or all keyword arguments above may be not supported.
     """
     ...
