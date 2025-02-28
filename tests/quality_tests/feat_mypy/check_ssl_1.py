3b42bf48aaf1e8d925a69fc2346617159dfcad81
Jos Verlinde <Jos.Verlinde@microsoft.com>
2025-02-28 12:51:11 +0100

    tests: Add MyPy quality tests
    Signed-off-by: Jos Verlinde <Jos.Verlinde@microsoft.com>

---
+++ tests/quality_tests/feat_mypy/check_ssl_1.py
@@ -0,0 +1,16 @@
+import ssl
+
+from typing_extensions import assert_type, get_type_hints
+
+a = ssl.CERT_NONE
+b = ssl.CERT_OPTIONAL
+c = ssl.CERT_REQUIRED
+
+# valid
+ctx = ssl.SSLContext(
+    protocol=ssl.PROTOCOL_TLS_SERVER  # stubs-ignore: board in ['rpi_pico_w'] or port == 'esp32'
+)
+
+# below should NOT be available in stubs
+# test wiith ignoring the error - which will raise an error in pyright / mypy if it is not needed
+not_there = ssl.create_default_context()  # type: ignore # stubs-ignore : linter == "mypy" and version < 1.24.0
---
+++ tests/quality_tests/feat_mypy/check_sys.py
@@ -0,0 +1,32 @@
+import sys
+
+bits = 0
+v = sys.maxsize
+while v:
+    bits += 1
+    v >>= 1
+if bits > 32:
+    # 64-bit (or more) platform
+    ...
+else:
+    # 32-bit (or less) platform
+    # Note that on 32-bit platform, value of bits may be less than 32
+    # (e.g. 31) due to peculiarities described above, so use "> 16",
+    # "> 32", "> 64" style of comparisons.
+    ...
+
+print(sys.version_info[0], sys.version_info[1], sys.version_info[2])
+print(sys.implementation)
+
+
+# Micropython Extensions
+exc = Exception("Foo")
+sys.print_exception(exc)  # stubs-ignore:  linter == "mypy"
+
+port = sys.platform
+if port in ["unix", "windows"]:
+
+    def byebye():
+        print("so long")
+
+    previous = sys.atexit(byebye)  # stubs-ignore:  linter == "mypy" or version < 1.24.0
