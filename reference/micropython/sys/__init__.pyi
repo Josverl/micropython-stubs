c25ad0a071fb6a0b5bcf64c3acee655faf5dc62d
Jos Verlinde <jos_verlinde@hotmail.com>
2025-03-14 16:30:20 +0100

    feat: add _mp_implementation class to represent MicroPython's sys.implementation
    closes #708
    
    Signed-off-by: Jos Verlinde <jos_verlinde@hotmail.com>

--- reference/micropython/sys/__init__.pyi
+++ reference/micropython/sys/__init__.pyi
@@ -12,14 +12,14 @@ from __future__ import annotations
 
 from typing import Callable, Dict, List, NoReturn, Tuple, overload
 
-from _mpy_shed import IOBase_mp
+from _mpy_shed import IOBase_mp, _mp_implementation
 from _typeshed import Incomplete
 
 argv: List
 """A mutable list of arguments the current program was started with."""
 byteorder: Incomplete
 """The byte order of the system (``"little"`` or ``"big"``)."""
-implementation: Incomplete
+implementation: _mp_implementation
 """\
 Object with information about the current Python implementation. For
 MicroPython, it has following attributes:
