c6e5f61dba42392150728f6fe6cb0128891ed20a
Jos Verlinde <Jos.Verlinde@microsoft.com>
2025-04-07 21:43:13 +0200

    reference/micropython/errno: Add errnos and descriptions
    Signed-off-by: Jos Verlinde <Jos.Verlinde@microsoft.com>

--- reference/micropython/errno/__init__.pyi
+++ reference/micropython/errno/__init__.pyi
@@ -7,42 +7,27 @@ CPython module: :mod:`python:errno` https://docs.python.org/3/library/errno.html
 
 This module provides access to symbolic error codes for `OSError` exception.
 A particular inventory of codes depends on :term:`MicroPython port`.
+
+Error codes, based on ANSI C/POSIX standard. All error codes start with
+"E". As mentioned above, inventory of the codes depends on
+:term:`MicroPython port`. Errors are usually accessible as ``exc.errno``
+where ``exc`` is an instance of `OSError`. Usage example::
+
+try:
+os.mkdir("my_dir")
+except OSError as exc:
+if exc.errno == errno.EEXIST:
+print("Directory already exists")
 """
+# https://www.thegeekstuff.com/2010/10/linux-error-codes/
 
 # source version: v1.24.0
 # origin module:: repos/micropython/docs/library/errno.rst
 from __future__ import annotations
 
-from typing import Dict
+from typing import Dict, Final
 
-from _typeshed import Incomplete
 
-EEXIST: Incomplete
-"""\
-Error codes, based on ANSI C/POSIX standard. All error codes start with
-"E". As mentioned above, inventory of the codes depends on
-:term:`MicroPython port`. Errors are usually accessible as ``exc.errno``
-where ``exc`` is an instance of `OSError`. Usage example::
-
-try:
-os.mkdir("my_dir")
-except OSError as exc:
-if exc.errno == errno.EEXIST:
-print("Directory already exists")
-"""
-EAGAIN: Incomplete
-"""\
-Error codes, based on ANSI C/POSIX standard. All error codes start with
-"E". As mentioned above, inventory of the codes depends on
-:term:`MicroPython port`. Errors are usually accessible as ``exc.errno``
-where ``exc`` is an instance of `OSError`. Usage example::
-
-try:
-os.mkdir("my_dir")
-except OSError as exc:
-if exc.errno == errno.EEXIST:
-print("Directory already exists")
-"""
 errorcode: Dict
 """\
 Dictionary mapping numeric error codes to strings with symbolic error
@@ -51,3 +36,50 @@ code (see above)::
 >>> print(errno.errorcode[errno.EEXIST])
 EEXIST
 """
+
+EPERM: Final[int] = 1
+"""Operation not permitted"""
+ENOENT: Final[int] = 2
+"""No such file or directory"""
+EIO: Final[int] = 5
+"""I/O error"""
+EBADF: Final[int] = 9
+"""Bad file descriptor"""
+EAGAIN: Final[int] = 11
+"""Resource temporarily unavailable"""
+ENOMEM: Final[int] = 12
+"""Out of memory"""
+EACCES: Final[int] = 13
+"""Permission denied"""
+EEXIST: Final[int] = 17
+"""File exists"""
+ENODEV: Final[int] = 19
+"""No such device"""
+EISDIR: Final[int] = 21
+"""Is a directory"""
+EINVAL: Final[int] = 22
+"""Invalid argument"""
+EOPNOTSUPP: Final[int] = 95
+"""Operation not supported"""
+EADDRINUSE: Final[int] = 98
+"""Address already in use"""
+ECONNABORTED: Final[int] = 103
+"""Connection aborted"""
+ECONNRESET: Final[int] = 104
+"""Connection reset by peer"""
+ENOBUFS: Final[int] = 105
+"""No buffer space available"""
+ENOTCONN: Final[int] = 107
+"""Transport endpoint is not connected"""
+ETIMEDOUT: Final[int] = 110
+"""Connection timed out"""
+ECONNREFUSED: Final[int] = 111
+"""Connection refused"""
+EHOSTUNREACH: Final[int] = 113
+"""Host is unreachable"""
+EALREADY: Final[int] = 114
+"""Operation already in progress"""
+EINPROGRESS: Final[int] = 115
+"""Operation now in progress"""
+ENOTSUP : Final[int] = ...
+"""Operation not supported"""
