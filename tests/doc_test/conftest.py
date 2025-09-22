387a31de6289068804289c8622baae903cc9ab04
Jos Verlinde <Jos_Verlinde@hotmail.com>
2025-09-22 17:04:26 +0200

    sybil can list codeblocks
    Signed-off-by: Jos Verlinde <Jos_Verlinde@hotmail.com>

---
+++ tests/doc_test/conftest.py
@@ -0,0 +1,38 @@
+from os import chdir, getcwd
+from pathlib import Path
+from shutil import rmtree
+from tempfile import mkdtemp
+
+import pytest
+from sybil import Sybil
+from sybil.parsers.codeblock import PythonCodeBlockParser
+from sybil.parsers.doctest import DocTestParser
+
+# @pytest.fixture(scope="module")
+# def tempdir():
+#     # there are better ways to do temp directories, but it's a simple example:
+#     path = mkdtemp()
+#     cwd = getcwd()
+#     try:
+#         chdir(path)
+#         yield path
+#     finally:
+#         chdir(cwd)
+#         rmtree(path)
+
+
+docs_path = (Path(__file__).parent / "micropython/docs")
+
+
+sybil = Sybil(
+    path=docs_path.as_posix(),
+    parsers=[
+        DocTestParser(),
+        PythonCodeBlockParser(future_imports=['print_function']),
+    ],
+    pattern='*.rst',
+    # fixtures=['tempdir']
+)
+
+
+pytest_collect_file = sybil.pytest()
