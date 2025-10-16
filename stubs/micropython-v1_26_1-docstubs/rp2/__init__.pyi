8ab01f1e214e0ba39d80c0c759b254df98f8beec
Josverl <Josverl@users.noreply.github.com>
2025-10-16 22:04:53 +0000

    Update docstubs stubs for stable

--- stubs/micropython-v1_26_1-docstubs/rp2/__init__.pyi
+++ stubs/micropython-v1_26_1-docstubs/rp2/__init__.pyi
@@ -22,14 +22,13 @@ from rp2.DMA import DMA
 from rp2.Flash import Flash
 from rp2.PIO import PIO
 from rp2.StateMachine import StateMachine
-from rp2 import PIOASMEmit, _PIO_ASM_Program, bootsel_button
-from rp2.PIOASMEmit import PIOASMEmit
 from machine import Pin
+from rp2.PIOASMEmit import PIOASMEmit
 from typing import Callable, List, Union, overload
 from micropython import const
+from rp2 import PIOASMEmit, _PIO_ASM_Program
 
 _PIO_ASM_Program: TypeAlias = PIOASMEmit
-
 class PIOASMError(Exception):
     """
     This exception is raised from `asm_pio()` or `asm_pio_encode()` if there is
