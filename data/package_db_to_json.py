4c72f473fac874baa38be50beeebf55296d77965
Jos Verlinde <Jos_Verlinde@hotmail.com>
2025-11-26 12:16:12 +0100

    refactor: Improve database path handling
    Signed-off-by: Jos Verlinde <Jos_Verlinde@hotmail.com>

--- data/package_db_to_json.py
+++ data/package_db_to_json.py
@@ -1,4 +1,4 @@
-# %%
+
 # Configure logging to output to the notebook
 import logging
 import sqlite3
@@ -9,19 +9,20 @@ import jsons
 logging.basicConfig(level=logging.INFO, format="%(levelname)s - %(message)s")
 log = logging.getLogger()
 
-# %%
 
-db_path = Path("../data/all_packages.db")
+if __file__:
+    db_path = Path(__file__).parent / "all_packages.db"
+else:
+    db_path = Path("../data/all_packages.db")
+
 if not db_path.exists():
-    db_path = Path("repos/micropython-stubs/data/all_packages.db")
-    if not db_path.exists():
-        raise FileNotFoundError("Database file not found")
+    raise FileNotFoundError("Database file not found")
 
 conn = sqlite3.connect(db_path)
 conn.row_factory = sqlite3.Row  # return rows as dicts
 cursor = conn.cursor()
 
-# %%
+
 SHORT_LIST = """
 SELECT DISTINCT name, mpy_version, port, board, variant FROM packages 
     where publish == TRUE
@@ -33,7 +34,7 @@ stublist = cursor.fetchall()
 
 log.info(f"found {len(stublist)} unique stub/version combinations")
 
-# %%
+
 
 
 schema = "https://raw.githubusercontent.com/Josverl/micropython-stubber/main/data/schema/packages-v1.0.0.json"
@@ -44,4 +45,4 @@ with open(db_path.with_name("stub-packages.json"), "w") as f:
     f.write(text)
 
 
-# %%
+
