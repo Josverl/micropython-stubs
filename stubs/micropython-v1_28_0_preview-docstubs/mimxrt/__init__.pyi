f11b5e91824a991b7f10acd3a3aa0c80dd38dbf6
Josverl <Josverl@users.noreply.github.com>
2026-01-27 22:03:47 +0000

    Update docstubs stubs for preview

---
+++ stubs/micropython-v1_28_0_preview-docstubs/mimxrt/__init__.pyi
@@ -0,0 +1,15 @@
+"""
+Functionality specific to NXP i.MXRT.
+
+MicroPython module: https://docs.micropython.org/en/v1.28.0/library/mimxrt.html
+
+The ``mimxrt`` module contains functions and classes specific to the NXP i.MXRT
+family of microcontrollers.
+"""
+
+# source version: v1.28.0-preview
+# origin module:: repos/micropython/docs/library/mimxrt.rst
+from __future__ import annotations
+from _typeshed import Incomplete
+from typing_extensions import TypeVar, TypeAlias, Awaitable
+from mimxrt.Flash import Flash
