bed7b6a2088e3a6d0f50434c6d43aed0f13b310a
Jos Verlinde <Jos_Verlinde@hotmail.com>
2026-02-24 23:11:14 +0100

    Add RPI_PICO_W v1.27.0
    Signed-off-by: Jos Verlinde <Jos_Verlinde@hotmail.com>

---
+++ stubs/micropython-v1_27_0-rp2-RPI_PICO_W/asyncio/lock.pyi
@@ -0,0 +1,16 @@
+"""
+Module: 'asyncio.lock' on micropython-v1.27.0-rp2-RPI_PICO_W
+"""
+
+# MCU: {'mpy': 'v6.3', 'build': '', 'ver': '1.27.0', 'arch': 'armv6m', 'version': '1.27.0', 'port': 'rp2', 'board': 'RPI_PICO_W', 'family': 'micropython', 'board_id': 'RPI_PICO_W', 'variant': '', 'cpu': 'RP2040'}
+# Stubber: v1.26.5
+from __future__ import annotations
+from typing import Generator
+from _typeshed import Incomplete
+
+class Lock:
+    def locked(self, *args, **kwargs) -> Incomplete: ...
+    def release(self, *args, **kwargs) -> Incomplete: ...
+    def acquire(*args, **kwargs) -> Generator:  ## = <generator>
+        ...
+    def __init__(self, *argv, **kwargs) -> None: ...
