c33b30065a90f78c59d45b8cbb327bc10d78c71c
Jos Verlinde <Jos_Verlinde@hotmail.com>
2026-02-24 23:11:14 +0100

    Add RPI_PICO_W v1.27.0
    Signed-off-by: Jos Verlinde <Jos_Verlinde@hotmail.com>

---
+++ stubs/micropython-v1_27_0-rp2-RPI_PICO_W/array.pyi
@@ -0,0 +1,13 @@
+"""
+Module: 'array' on micropython-v1.27.0-rp2-RPI_PICO_W
+"""
+
+# MCU: {'mpy': 'v6.3', 'build': '', 'ver': '1.27.0', 'arch': 'armv6m', 'version': '1.27.0', 'port': 'rp2', 'board': 'RPI_PICO_W', 'family': 'micropython', 'board_id': 'RPI_PICO_W', 'variant': '', 'cpu': 'RP2040'}
+# Stubber: v1.26.5
+from __future__ import annotations
+from _typeshed import Incomplete
+
+class array:
+    def extend(self, *args, **kwargs) -> Incomplete: ...
+    def append(self, *args, **kwargs) -> Incomplete: ...
+    def __init__(self, *argv, **kwargs) -> None: ...
---
+++ stubs/micropython-v1_27_0-rp2-RPI_PICO_W/json.pyi
@@ -0,0 +1,13 @@
+"""
+Module: 'json' on micropython-v1.27.0-rp2-RPI_PICO_W
+"""
+
+# MCU: {'mpy': 'v6.3', 'build': '', 'ver': '1.27.0', 'arch': 'armv6m', 'version': '1.27.0', 'port': 'rp2', 'board': 'RPI_PICO_W', 'family': 'micropython', 'board_id': 'RPI_PICO_W', 'variant': '', 'cpu': 'RP2040'}
+# Stubber: v1.26.5
+from __future__ import annotations
+from _typeshed import Incomplete
+
+def loads(*args, **kwargs) -> Incomplete: ...
+def load(*args, **kwargs) -> Incomplete: ...
+def dumps(*args, **kwargs) -> Incomplete: ...
+def dump(*args, **kwargs) -> Incomplete: ...
