9be91e5eb67817f16a4ef57744e810ad932b446c
Jos Verlinde <Jos_Verlinde@hotmail.com>
2026-04-12 00:05:23 +0200

    v1.28.0 :  micropython-esp32-esp32_generic-stubs
    Signed-off-by: Jos Verlinde <Jos_Verlinde@hotmail.com>

---
+++ stubs/micropython-v1_28_0-esp32-ESP32_GENERIC/uplatform.pyi
@@ -0,0 +1,13 @@
+"""
+Module: 'uplatform' on micropython-v1.28.0-esp32-ESP32_GENERIC
+"""
+
+# MCU: {'variant': '', 'build': '', 'arch': 'xtensawin', 'port': 'esp32', 'board': 'ESP32_GENERIC', 'board_id': 'ESP32_GENERIC', 'mpy': 'v6.3', 'ver': '1.28.0', 'family': 'micropython', 'cpu': 'ESP32', 'version': '1.28.0'}
+# Stubber: v1.28.0
+from __future__ import annotations
+from typing import Any, Final, Generator, AsyncGenerator
+from _typeshed import Incomplete
+
+def platform(*args, **kwargs) -> Incomplete: ...
+def python_compiler(*args, **kwargs) -> Incomplete: ...
+def libc_ver(*args, **kwargs) -> Incomplete: ...
