b1c46b07835029d5d42b4d24ca56a4dda39a299f
Jos Verlinde <Jos_Verlinde@hotmail.com>
2026-05-03 14:57:32 +0200

    v1.28.0: Add Windows stubs
    Signed-off-by: Jos Verlinde <Jos_Verlinde@hotmail.com>

---
+++ stubs/micropython-v1_28_0-windows-standard/random.pyi
@@ -0,0 +1,15 @@
+"""
+Module: 'random' on micropython-v1.28.0-windows-standard
+"""
+# MCU: {'family': 'micropython', 'version': '1.28.0', 'build': '', 'ver': '1.28.0', 'port': 'windows', 'board': 'standard', 'board_id': 'standard', 'variant': '', 'cpu': 'win32 [GCC 12.0.0] version', 'mpy': 'v6.3', 'arch': ''}
+# Stubber: v1.28.3
+from __future__ import annotations
+from typing import Any, Final, Generator, AsyncGenerator
+from _typeshed import Incomplete
+
+def seed(*args, **kwargs) -> Incomplete:
+    ...
+
+def getrandbits(*args, **kwargs) -> Incomplete:
+    ...
+
