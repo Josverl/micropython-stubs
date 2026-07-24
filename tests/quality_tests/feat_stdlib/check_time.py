4eec2214f54902c0840f874fb1ac53d875cddb8f
Jos Verlinde <Jos_Verlinde@hotmail.com>
2026-07-24 11:41:55 +0200

    QA: Ignore the time arithmitic checks for stubs prior to v1.28.0.
    Signed-off-by: Jos Verlinde <Jos_Verlinde@hotmail.com>

--- tests/quality_tests/feat_stdlib/check_time.py
+++ tests/quality_tests/feat_stdlib/check_time.py
@@ -11,42 +11,49 @@ seconds = time.mktime(local_time)  # stubs-ignore:  version < 1.24.0
 from time import ticks_ms, ticks_diff, ticks_add
 from sys import print_exception
 
+
 def anything():
     timeout_at = ticks_add(ticks_ms(), 500)
-    while ticks_diff((now:=ticks_ms()), timeout_at) < 0:
+    while ticks_diff((now := ticks_ms()), timeout_at) < 0:
         # do stuff
         pass
     try:
-        print('timeout', now)
-    except Exception as e:              # thus throwing an…
-        print_exception(e)              # …AttributeError
+        print("timeout", now)
+    except Exception as e:  # thus throwing an…
+        print_exception(e)  # …AttributeError
 
 
 # -----------------------
+# Guard against direct tick arethmatic overflow,
+# which is a common source of bugs in MicroPython code.
 
 from time import ticks_ms
-start=ticks_ms()
-while ticks_ms()-start<100: # type: ignore # will fail if the type: ignore is not required
+
+start = ticks_ms()
+# will fail if the type: ignore is not required
+while ticks_ms() - start < 100:  # type: ignore # stubs-ignore: version<1.28.0
     pass
     # if uart.any():  return uart.read().decode()
-print('timeout', ticks_ms()-start) # type: ignore # will fail if the type: ignore is not required
+# will fail if the type: ignore is not required
+print("timeout", ticks_ms() - start)  # type: ignore # stubs-ignore: version<1.28.0
 # return uart.read().decode()
 
 # -----------------------
-cycle=1
-gotcha=now0=was0=d0=0
-was=ticks_ms()
+cycle = 1
+gotcha = now0 = was0 = d0 = 0
+was = ticks_ms()
 while 1:
-    now=ticks_ms()
-    d=now-was       #  type: ignore #  will fail if the type: ignore is not required
-    cycle+=1
-    if d>100:
-      if gotcha==0:  
-        print( 'TIMEOUT  prev', now0, was0, d0, now, was, d, end=' ')
-        gotcha=cycle
-    if gotcha==cycle-1: 
-        print('  post', now, was, d)
+    now = ticks_ms()
+    # will fail if the type: ignore is not required
+    d = now - was  # type: ignore # stubs-ignore: version<1.28.0
+    cycle += 1
+    if d > 100:
+        if gotcha == 0:
+            print("TIMEOUT  prev", now0, was0, d0, now, was, d, end=" ")
+            gotcha = cycle
+    if gotcha == cycle - 1:
+        print("  post", now, was, d)
         break
-    now0=now
-    was0=was
-    d0=d
+    now0 = now
+    was0 = was
+    d0 = d
