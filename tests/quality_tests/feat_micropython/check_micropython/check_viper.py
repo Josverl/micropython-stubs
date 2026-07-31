697d32a1210cbcb16b321dd5bb5aa3f78b53c89a
Jos Verlinde <Jos_Verlinde@hotmail.com>
2026-08-01 00:53:21 +0200

    tests: Update tests and config for builtins
    - update viper tests
    - ruff check - needs a section in pyproject.toml
    
    Signed-off-by: Jos Verlinde <Jos_Verlinde@hotmail.com>

--- tests/quality_tests/feat_micropython/check_micropython/check_viper.py
+++ tests/quality_tests/feat_micropython/check_micropython/check_viper.py
@@ -1,15 +1,20 @@
 import micropython
 
-# https://docs.micropython.org/en/v1.20.0/reference/speed_python.html?highlight=viper#the-viper-code-emitter
-# Casting operators are currently: int, bool, uint, ptr, ptr8, ptr16 and ptr32.
-# todo: micropython.viper - add support for casting operators
+
+@micropython.viper
+def unsigned_word() -> uint:
+    return 0xFFFFFFFF
 
 
 @micropython.viper
-def foo(self, arg: int) -> int:
-    buf = ptr8(self.linebuf)  # type: ignore 
-    for x in range(20, 30):
-        bar = buf[x]  # Access a data item through the pointer
-        # code omitted
-
-    return len(buf)  # Return an integer value
+def read_viper(buffer: bytearray, offset: int, address: ptr) -> uint:
+    byte_pointer = ptr8(buffer)
+    halfword_pointer = ptr16(buffer)
+    word_pointer = ptr32(address)
+    unsigned = uint(byte_pointer[offset])
+    byte_pointer[offset] = unsigned
+    halfword_pointer[offset] = unsigned
+    word_pointer[offset] = unsigned
+    value: int = byte_pointer[offset]
+    value = unsigned
+    return unsigned
