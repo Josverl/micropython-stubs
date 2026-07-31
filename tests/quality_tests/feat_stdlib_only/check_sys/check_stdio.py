a88b2b1e438f8fea5be2adff2d1ce8fde74348ae
Jos Verlinde <Jos_Verlinde@hotmail.com>
2026-08-01 00:00:10 +0200

    fix: IO and BinaryIO missing methods.
    closes: #882
    Signed-off-by: Jos Verlinde <Jos_Verlinde@hotmail.com>

--- tests/quality_tests/feat_stdlib_only/check_sys/check_stdio.py
+++ tests/quality_tests/feat_stdlib_only/check_sys/check_stdio.py
@@ -1,6 +1,7 @@
 import sys
+from typing import BinaryIO
 
-# verify if stdout and stderr acceppt bytes output 
+# verify if stdout and stderr acceppt bytes output
 sys.stdout.write("foofoo")
 sys.stdout.write(b"bar")
 sys.stdout.flush()
@@ -9,3 +10,34 @@ sys.stdout.flush()
 sys.stderr.write("foofoo")
 sys.stderr.write(b"bar")
 sys.stderr.flush()
+
+
+class Poller:
+    def register(self, stream: BinaryIO, event: int) -> None: ...
+
+
+class RemoteCommand:
+    def __init__(self, poller: Poller) -> None:
+        self.buf4 = bytearray(4)
+        self.fout = sys.stdout.buffer
+        self.fin = sys.stdin.buffer
+        self.poller = poller
+        self.poller.register(self.fin, 1)
+
+    def rd_into(self, buf: bytearray, length: int) -> None:
+        read = self.fin.readinto(buf, length)
+        if read is None:
+            return
+        view = memoryview(buf)
+        while read < length:
+            count = self.fin.readinto(view[read:], length - read)
+            if count is None:
+                return
+            read += count
+
+    def begin(self, command_type: int) -> None:
+        self.buf4[0] = 0x18
+        self.buf4[1] = command_type
+        self.fout.write(self.buf4, 2)
+        self.fin.readinto(self.buf4)
+        self.fin.readinto(self.buf4, 1)
