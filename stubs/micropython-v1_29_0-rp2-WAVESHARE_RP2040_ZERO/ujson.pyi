fc686c2b46de778dc6f09b84772104a85410443e
Jos Verlinde <Jos_Verlinde@hotmail.com>
2026-08-29 09:07:21 +0200

    v1.29.0: Add rp2 WAVESHARE_RP2040_ZERO.
    Signed-off-by: Jos Verlinde <Jos_Verlinde@hotmail.com>

---
+++ stubs/micropython-v1_29_0-rp2-WAVESHARE_RP2040_ZERO/ujson.pyi
@@ -0,0 +1,16 @@
+"""
+Module: 'ujson' on micropython-v1.29.0-rp2-WAVESHARE_RP2040_ZERO
+"""
+
+# MCU: {'mpy': 'v6.3', 'build': '', 'ver': '1.29.0', 'arch': 'armv6m', 'version': '1.29.0', 'port': 'rp2', 'board': 'WAVESHARE_RP2040_ZERO', 'family': 'micropython', 'board_id': 'WAVESHARE_RP2040_ZERO', 'variant': '', 'cpu': 'RP2040'}
+# Stubber: v1.28.6
+from __future__ import annotations
+
+from typing import Any, AsyncGenerator, Final, Generator
+
+from _typeshed import Incomplete
+
+def loads(*args, **kwargs) -> Incomplete: ...
+def load(*args, **kwargs) -> Incomplete: ...
+def dumps(*args, **kwargs) -> Incomplete: ...
+def dump(*args, **kwargs) -> Incomplete: ...
